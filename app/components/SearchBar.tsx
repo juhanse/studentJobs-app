757d09bf253e40037a70e9225d8acb78bdf6da7d
juhanse <julien.hanse12@gmail.com>
2025-01-23 00:39:12 +0100

    fix: style search bar

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { SafeAreaView, TextInput, StyleSheet, Pressable } from 'react-native';
 
 const SearchBar = () => {
+	const [search, setSearch] = useState('');
+
+	if (search)
+		console.log(search);
+
 	return (
-		<View style={styles.container}>
+		<SafeAreaView style={styles.container}>
 			<TextInput
 				style={styles.searchInput}
 				placeholder="Search..."
+				placeholderTextColor="white"
+				value={search}
+				onChangeText={setSearch}
 			/>
-		</View>
+			<Pressable style={styles.searchIcon}>
+				<Ionicons name="search" size={24} color="white" />
+			</Pressable>
+		</SafeAreaView>
 	);
 };
 
 const styles = StyleSheet.create({
 	container: {
-		width: '90%',
-		marginTop: 10,
-		alignSelf: 'center',
+		flex: 1,
 	},
 	searchInput: {
 		height: 40,
@@ -24,6 +34,17 @@ const styles = StyleSheet.create({
 		borderWidth: 2,
 		borderRadius: 10,
 		paddingHorizontal: 10,
+		fontSize: 16,
+	},
+	searchIcon: {
+		position: 'absolute',
+		right: 10,
+		top: 7,
+	},
+	text: {
+		fontSize: 24,
+		fontWeight: 'bold',
+		color: '#333',	
 	},
 });
 
