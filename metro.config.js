75dd41a6400fafd862147b0c30f384991ecbcb66
juhanse <julien.hanse12@gmail.com>
2025-01-29 22:45:25 +0100

    fix: config svg module

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,17 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const defaultConfig = getDefaultConfig(__dirname);
 
+defaultConfig.transformer = {
+	...defaultConfig.transformer,
+	babelTransformerPath: require.resolve('react-native-svg-transformer'),
+};
+  
+defaultConfig.resolver = {
+	...defaultConfig.resolver,
+	assetExts: defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg'),
+	sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
+};
+
 defaultConfig.resolver.sourceExts.push('cjs');
 
 module.exports = defaultConfig;
